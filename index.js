0b81e718c65be36b1b4b1f649b83a3d63ad151c9
p-mendoncad <125631065+p-mendoncad@users.noreply.github.com>
2023-12-01 19:35:38 -0300

    implementação dos dados

---
+++ index.js
@@ -0,0 +1,36 @@
+function d4(){
+    let res_d4 = Math.floor(Math.random()*4)+1;
+    document.getElementById("res_d4").innerHTML = `${res_d4}`
+}
+
+function d6(){
+    let res_d6 = Math.floor(Math.random()*6)+1;
+    document.getElementById("res_d6").innerHTML = `${res_d6}`
+}
+
+function d8(){
+    let res_d8 = Math.floor(Math.random()*8)+1;
+    document.getElementById("res_d8").innerHTML = `${res_d8}`
+}
+
+function d10(){
+    let res_d10 = Math.floor(Math.random()*10);
+    document.getElementById("res_d10").innerHTML = `${res_d10}`
+}
+
+function d12(){
+    let res_d12 = Math.floor(Math.random()*12)+1;
+    document.getElementById("res_d12").innerHTML = `${res_d12}`
+}
+
+
+function d20(){
+    let res_d20 = Math.floor(Math.random()*20)+1;
+    document.getElementById("res_d20").innerHTML = `${res_d20}`
+}
+
+
+function d100(){
+    let res_d100 = Math.floor(Math.random()*100)+1;
+    document.getElementById("res_d100").innerHTML = `${res_d100}`
+}
